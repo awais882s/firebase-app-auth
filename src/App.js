db4f96f7956729e38ebc24ddc3c576b78711613a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 12:22:45 +0500

    del login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
 import "./scss/style.scss";
 function App() {
   return (
     <>
       <Header />
+      <Login />
       <Footer />
 
     </>
