27a5d0ae8e103e3ff8aae5341dd70230ae7810e0
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 12:41:21 +0500

    stylings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Login from "./components/Login/Login";
 import "./scss/style.scss";
 function App() {
   return (
     <>
       <Header />
-      <Login />
       <Footer />
 
     </>
