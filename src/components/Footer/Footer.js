cf36132e9ddfb2656c3f68b1be21b0b43c8c7519
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 11:23:08 +0500

    folder structures

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <p>Copyright &copy; 2020 <a href="https://muhammad-awais-house.business.site/"></a>Muhammad Awais </p>
+            </footer>
+
+        </>
+
+
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+
+  return (
+    <>
+    <header>
+        <main>
+            
+        </main>
+    </header>
+    </>
+  )
+}
